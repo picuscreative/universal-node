ceca67277e981f6311c942ca61db0a822789c74e
David Cunha <davidgoncalvescunha@gmail.com>
2018-05-13 21:40:17 +0100

    fix: remove name of screenshots for testing, add comment to import grid

--- tests/about.js
+++ tests/about.js
@@ -7,5 +7,5 @@ const serverURL = config.app.site_url;
 fixture('As an User I want to able to see the About page so I can read more about the company').page(`${serverURL}/about`);
 
 test('About', async (t) => {
-  await t.expect(Selector('h1').innerText).eql('PICUS Creative').takeScreenshot('about');
+  await t.expect(Selector('h1').innerText).eql('PICUS Creative').takeScreenshot();
 });
--- tests/index.js
+++ tests/index.js
@@ -7,5 +7,5 @@ const serverURL = config.app.site_url;
 fixture('As an User I want to able to see the Homepage so I can get started').page(serverURL);
 
 test('Index', async (t) => {
-  await t.expect(Selector('p').innerText).eql('Hello johndoe@mail.com').takeScreenshot('index');
+  await t.expect(Selector('p').innerText).eql('Hello johndoe@mail.com').takeScreenshot();
 });

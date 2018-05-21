6ac0cd73c0cc50b4eac2f4d66bd5111d203a7cb5
David Cunha <davidgoncalvescunha@gmail.com>
2018-05-21 16:35:22 +0100

    refactor: remove unecessary dependency es6-promise

--- client/actions/user.js
+++ client/actions/user.js
@@ -1,8 +1,6 @@
-import es6promise from 'es6-promise';
 import fetch from 'isomorphic-fetch';
 import types from '../actions/types';
 
-es6promise.polyfill();
 const apiURL = process.env.REACT_APP_API_URL;
 
 export default {

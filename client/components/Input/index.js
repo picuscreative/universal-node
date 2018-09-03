535bd69a12e4f2a72b127674f83118474eeeacf1
PICUS Lda <rui@picus.pt>
2018-09-03 15:37:57 +0100

    feat: added input component

---
+++ client/components/Input/index.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import styles from './styles.css';
+
+/**
+ * Input:
+ * Component that renders a text inside an h1,
+ * intended to be used as a title
+ */
+class Input extends Component {
+  static propTypes = {
+    /**
+     * Class name for the container
+     */
+    className: PropTypes.string,
+    /**
+     * Text that will appear in the element
+     */
+    placeholder: PropTypes.string,
+    /**
+     * Allows to add a fixed padding
+     */
+    withPadding: PropTypes.bool,
+    /**
+     * Allows to add a fixed padding top
+     */
+    withPaddingTop: PropTypes.bool,
+    /**
+     * Allows the alignment of the element to the center
+     */
+    alignCenter: PropTypes.bool,
+    /**
+     * Definition of the input type
+     */
+    type: PropTypes.string,
+    /**
+     * Value state setter
+     */
+    handleChange: PropTypes.func,
+    /**
+     * Name to be given to identify the input element
+     */
+    name: PropTypes.string.isRequired,
+    /**
+     * Change style if input has an error condition
+     */
+    error: PropTypes.bool,
+    /**
+     * Value to be given to the input element
+     */
+    value: PropTypes.string,
+    /**
+     * Pattern for validating in case of HTML5
+     */
+    pattern: PropTypes.string,
+  };
+
+  render() {
+    const {
+      className,
+      placeholder,
+      withPadding,
+      withPaddingTop,
+      alignCenter,
+      type,
+      handleChange,
+      name,
+      error,
+      value,
+    } = this.props;
+
+    return (
+      <input
+        name={name}
+        onChange={handleChange}
+        className={classNames(
+          className,
+          styles.input,
+          withPadding && styles.padding,
+          withPaddingTop && styles['padding-top'],
+          alignCenter && styles.alignCenter,
+          error && styles.error,
+        )}
+        type={type || 'text'}
+        placeholder={placeholder}
+        value={value}
+      />
+    );
+  }
+}
+
+export default Input;
--- pages/form.js
+++ pages/form.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import Input from '~/components/Input';
 import App from '../client/components/App';
 import CustomHead from '../client/components/CustomHead';
 
@@ -20,13 +21,31 @@ class FormAndInputs extends PureComponent {
     meta: PropTypes.object.isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
     const { meta } = this.props;
+    const { name } = this.state;
     return (
       <App>
         <CustomHead title={meta.title} description={meta.description} />
         <h1>Form & Input Examples</h1>
         <p>Demonstration usage of the form elements:</p>
+        <Input name="name" handleChange={this.handleChange} />
+        <label>{name ? `Hey, now I'm named ${name}` : 'I have no name :('}</label>
       </App>
     );
   }

cbd9bcf5c7add655ddd20c115b27ecebfe866320
PICUS Lda <rui@picus.pt>
2018-09-04 11:02:21 +0100

    feat: added svg component

--- client/components/Svg/doc.md
+++ client/components/Svg/doc.md
@@ -9,7 +9,11 @@ Inline svg content inside `<i>`
 prop | type | default | required | description
 ---- | :----: | :-------: | :--------: | -----------
 **className** | `String` |  | :x: | Class name for `<i>`
+**id** | `String` |  | :x: | Id name for `<i>`
+**onClick** | `(custom validator)` |  | :x: | On click action callback
+**selected** | `Boolean` |  | :x: | Svg selection
 **svg** | `String` |  | :white_check_mark: | Svg content
+**svgHover** | `String` |  | :x: | Svg hover content
 
 
 
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import App from '../client/components/App';
-import CustomHead from '../client/components/CustomHead';
-import Svg from '../client/components/Svg';
-import logo from '../client/shared/media/images/picus-logo.jpeg';
-import thumbsUp from '../client/shared/media/images/icons/thumbs-up.svg';
+import App from '~/components/App';
+import CustomHead from '~/components/CustomHead';
+import Svg from '~/components/Svg';
+import logo from '~/shared/media/images/picus-logo.jpeg';
+import thumbsUp from '~/shared/media/images/icons/thumbs-up.svg';
+import styles from '~/shared/styles/pages/about.scss';
 
 class About extends PureComponent {
   static async getInitialProps() {
@@ -20,15 +21,12 @@ class About extends PureComponent {
     const { meta } = this.props;
     return (
       <App>
-        <CustomHead
-          title={ meta.title }
-          description={ meta.description }
-        />
+        <CustomHead title={meta.title} description={meta.description} />
         <h1>PICUS Creative</h1>
         <p>BRINGING AMAZING DIGITAL PRODUCTS TO LIFE.</p>
-        <Svg className="icon" svg={ thumbsUp } />
+        <Svg className={styles.icon} svg={thumbsUp} />
         <a href="http://picuscreative.com" target="_blank" rel="noopener noreferrer">
-          <img width="100px" src={ logo } alt="PICUS" />
+          <img width="100px" src={logo} alt="PICUS" />
         </a>
       </App>
     );
